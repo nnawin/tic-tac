6d4a48f09895e4a58633ba0e7b4c0a81c88bf55f
nnawin <nnawin@gmail.com>
2020-06-03 16:13:16 +0800

    Updates

---
+++ precache-manifest.519ae22be866ba088d20d31b94ba95a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f01c70a5a4a24125635e9b469b7cd8",
+    "url": "/tic-tac/index.html"
+  },
+  {
+    "revision": "dc6bffa0af8034ac6581",
+    "url": "/tic-tac/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "c907fc87b0165d7b18ec",
+    "url": "/tic-tac/static/js/2.927740f4.chunk.js"
+  },
+  {
+    "revision": "dc6bffa0af8034ac6581",
+    "url": "/tic-tac/static/js/main.4d7ad87a.chunk.js"
+  },
+  {
+    "revision": "861916c6ed54173d9140",
+    "url": "/tic-tac/static/js/runtime~main.428177d8.js"
+  }
+]);
